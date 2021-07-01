3d5685deb0d81b86902fc71d0f9ec3c23a38c73f
SeredaM <75209816+SeredaM@users.noreply.github.com>
2021-07-01 19:15:32 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct tree {
+	int id;
+	int weight;
+	struct tree *left, *right;
+} tree;
+
+
+
+void push_tree(tree **T,int id)
+{
+  tree *ptr=(tree*)malloc(sizeof(tree));
+  ptr->id=id;
+  ptr->left=NULL;
+  ptr->right=NULL;
+  int w=rand()%3;
+  if (w==0)
+   ptr->weight=-1;
+  if (w==1)
+   ptr->weight=0;
+  if (w==2)
+   ptr->weight=1;
+
+  if ((*T)==NULL)
+  {
+    *T=ptr;
+    return;
+  }
+    tree *tmp=*T;
+    while (1==1)
+    {
+
+      if (id<tmp->id)
+      {
+          if (tmp->left==NULL)
+          {
+              tmp->left=ptr;
+              return;
+          }
+            tmp=tmp->left;
+      }
+      else
+      {
+        if (tmp->right==NULL)
+          {
+              tmp->right=ptr;
+              return;
+          }
+        tmp=tmp->right;
+      }
+    }
+}
+
+void del (tree **T,int del_id)
+{
+   tree *del_link=*T;
+   tree *parent;
+   while (del_id!=del_link->id)
+   {
+       if (del_id<del_link->id)
+       {
+        parent=del_link;
+        del_link=del_link->left;
+       }
+       else
+       {
+        parent=del_link;
+        del_link=del_link->right;
+       }
+   }
+   if ((del_link->left==NULL)&&(del_link->right==NULL))
+   {
+       if (parent->left == del_link)
+       parent->left=NULL;
+       else
+       parent->right=NULL;
+       free(del_link);
+       return;
+   }
+   if (del_link->left==NULL)
+   {
+       if (parent->left == del_link)
+       parent->left=del_link->right;
+       else
+       parent->right=del_link->right;
+       return;
+   }
+   if (del_link->right==NULL)
+   {
+       if (parent->left == del_link)
+       parent->left=del_link->left;
+       else
+       parent->right=del_link->left;
+       return;
+   }
+   tree *min=del_link->right;
+   while (min->left!=NULL)
+    min=min->left;
+    printf("%d\n",min->id);
+    int t=min->id;
+   del(T,t);
+   del_link->id=t;
+
+
+}
+
+
+void show(tree *t, int n) {
+    char s[10];
+    itoa(n*3 ,s, 10);
+    char ss[] = "%";
+    strcat(ss, s);
+    strcat(ss, "d\n"); //ss=Ф% число d\nФ
+	if (t != NULL)
+    {
+		show(t->left, n + 1);
+		printf(ss, t->id);
+		show(t->right, n + 1);
+	}
+
+}
+void show_weights(tree *T)
+{
+    if (T==NULL)
+        return;
+    printf("%d(%d)\n",T->id,T->weight);
+    show_weights(T->left);
+    show_weights(T->right);
+
+}
+void neg_leaves(tree *T,tree *parent)
+{
+    if (T==NULL)
+        return;
+    if ((T->left==NULL)&&(T->right==NULL)&&(T->weight==-1))
+    {
+        if (parent->left == T)
+       parent->left=NULL;
+       else
+       parent->right=NULL;
+       free(T);
+        return;
+    }
+    neg_leaves(T->left,T);
+    neg_leaves(T->right,T);
+}
+
+
+void main()
+{
+  tree *T=NULL;
+  int N=10,id,n;
+  for(int i=0;i<N;i++)
+    push_tree(&T,abs(rand()%100));
+  while (n!=6)
+  {
+      printf("1. Show tree\n");
+      printf("2. Add element\n");
+      printf("3. Delete element\n");
+      printf("4. Show element weights\n");
+      printf("5. Delete leaves with negative weights\n");
+      printf("6. Exit\n");
+      scanf("%d",&n);
+      switch(n)
+      {
+      case 1:
+       show(T,4);
+       break;
+      case 2:
+        printf("Enter element ID\n");
+        scanf("%d",&id);
+        push_tree(&T,id);
+        break;
+      case 3:
+        printf("Enter element ID\n");
+        scanf("%d",&id);
+        del(&T,id);
+        break;
+      case 4:
+        show_weights(T);
+        break;
+      case 5:
+          neg_leaves(T,NULL);
+          break;
+      }
+  }
+}
+
